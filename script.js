852d5c2d072e88641842eb7a075ec132fb362277
lyjoice <lahely.na@gmail.com>
2024-10-17 00:14:13 +0200

    pages d'inscription et de connection crées avec css

--- script.js
+++ script.js
@@ -50,14 +50,37 @@ function registerUser(){
     const username= document.getElementById("username").value
     const password= document.getElementById("password").value
 
-    if(username===storedUsername&&password===storedPassword){
-        /*localStorage fonctionne avec clé:valeur*/
-        localStorage.setItem("isAuthenticated", true)
+    if(username&&password){
+        /*localStorage fonctionne avec clé,valeur*/
+        localStorage.setItem("username", username)
         localStorage.setItem("password", password)
    
-        alert("inscription réussie! Vous pouvez vous connecter")
+        alert("inscription réussie! Vous pouvez maintenant vous connecter")
     }else{
         alert("Veuillez remplir tous les champs")
     }
 }
-/*récupérer données sur le dom qui est chargé*/
+/*LOGIN*/
+function loginUser(){
+    const username= document.getElementById("login-username").value
+    const password= document.getElementById("login-password").value
+
+    const storedUsername=localStorage.getItem("username")
+    const storedPassword=localStorage.getItem("password")
+
+    if(username===storedUsername && password===storedPassword){
+        localStorage.setItem("isAuthenticated", true)
+        window.location.href="index.html"
+    }else{
+        alert("nom d'utilisateur ou mot de pass incorrect")
+    }
+}
+
+function checkAuth(){
+    const isAuthenticated =localStorage.getItem("isAuthenticated")
+    if(isAuthenticated !== "true"){
+        alert("Veuillez vous connecter pour acceder au quiz") 
+        window.location.href="login.html"
+    }
+}
+
