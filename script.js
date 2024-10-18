b607151540489d62ed38d5a2b4896e4134f6644d
lyjoice <lahely.na@gmail.com>
2024-10-18 03:14:46 +0200

    modif des balises script dans les fichiers login.html et register.html

--- script.js
+++ script.js
@@ -48,50 +48,13 @@ function submitQuiz() {
         })
     })
 }
-/*REGISTER*/
-function registerUser(){
-    const username= document.getElementById("username").value
-    const password= document.getElementById("password").value
-
-    if(username&&password){
-        /*localStorage fonctionne avec clé,valeur*/
-        localStorage.setItem("username", username)
-        localStorage.setItem("password", password)
-   
-        alert("inscription réussie! Vous pouvez maintenant vous connecter")
-    }else{
-        alert("Veuillez remplir tous les champs")
-    }
-}
-/*LOGIN*/
-function loginUser(){
-    const username= document.getElementById("login-username").value
-    const password= document.getElementById("login-password").value
-
-    const storedUsername=localStorage.getItem("username")
-    const storedPassword=localStorage.getItem("password")
-
-    if(username===storedUsername && password===storedPassword){
-        localStorage.setItem("isAuthenticated", true)
-        window.location.href="index.html"
-    }else{
-        alert("nom d'utilisateur ou mot de pass incorrect")
-    }
-}
-function checkAuth(){
-    const isAuthenticated =localStorage.getItem("isAuthenticated")
-    if(isAuthenticated !== "true"){
-        alert("Veuillez vous connecter pour acceder au quiz") 
-        window.location.href="login.html"
-    }
-}
 /**
  * cette function affiche username dans le span, le nom utilisateur du localStorage
  * @param {*} username 
  */
 function showUserMenu(username){
     const usernameDisplay= document.getElementById("username-display")
-    usernameDisplay.textContent= username
+    usernameDisplay.textContent= (username)
 }
 /*Unefois le DOM chargé, la fonction récupère l'username*/
 document.addEventListener("DOMContentLoaded", function(){
@@ -105,5 +68,7 @@ document.addEventListener("DOMContentLoaded", function(){
 
 document.getElementById("logout-btn").addEventListener("click", function(){
     localStorage.removeItem("username")
+    localStorage.removeItem("password")
+    const isAuthenticated =localStorage.getItem("isAuthenticated",false)
     window.location.href="login.html"
 })
