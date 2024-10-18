4674fa3fb3836b7537ae2d79a4e49b0a06996fab
lyjoice <lahely.na@gmail.com>
2024-10-19 01:02:30 +0200

    code js fonctionnel pour inscription connection  deconnection

--- index.js
+++ index.js
@@ -25,3 +25,66 @@ if (score === 3) {
     resultDIV.innerHTML += "<br>Vous pouvez faire mieux!"
 }
 }
+//REGISTER
+function registerUser(){
+    const username= document.getElementById("username").value
+    const password= document.getElementById("password").value
+
+    if(username&&password){
+        /*localStorage fonctionne avec clé,valeur*/
+        localStorage.setItem("username", username)
+        localStorage.setItem("password", password)
+   
+        alert("inscription réussie! Vous pouvez maintenant vous connecter")
+        window.location.href="login.html"
+    }else{
+        alert("Veuillez remplir tous les champs")
+    }
+}
+//LOGIN expliquer la f, ses paramètres,types, ce qu'elle renvoie
+function loginUser(){
+    const username= document.getElementById("login-username").value
+    const password= document.getElementById("login-password").value
+
+    const storedUsername=localStorage.getItem("username")
+    const storedPassword=localStorage.getItem("password")
+
+    if(username===storedUsername && password===storedPassword){
+        localStorage.setItem("isAuthenticated", true)
+        window.location.href="index.html"
+    }else{
+        alert("nom d'utilisateur ou mot de pass incorrect")
+    }
+}
+/**
+ * cette fonction affiche username dans le span
+ * @param {*} username 
+ */
+function showUserMenu(username){
+    const usernameDisplay= document.getElementById("username-display")
+    usernameDisplay.textContent= username
+}
+//Unefois le DOM chargé, la fonction récupère l'username dans le localStorage
+document.addEventListener("DOMContentLoaded", ()=>{
+    const storedUsername= localStorage.getItem("username")
+    if(storedUsername){
+        showUserMenu(storedUsername)
+    }else{
+        window.location.href= "login.html"
+    }
+})
+document.getElementById("logout-btn").addEventListener("click", function(){
+    localStorage.removeItem("username")
+    localStorage.removeItem("password")
+    localStorage.setItem("isAuthenticated", false)
+    window.location.href= "login.html"
+})
+
+
+
+/*function checkAuth(){
+    const isAuthenticated =localStorage.getItem("isAuthenticated")
+    if(isAuthenticated !== "true"){
+        alert("Veuillez vous connecter pour acceder au quiz") 
+    }
+}*/
